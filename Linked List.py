53f1e14973a919e761f7e775b95a025e408dd730
Olugbenga Ojo <60803514+UltimateOjo@users.noreply.github.com>
2021-06-12 10:56:19 -0500

    Add files via upload

---
+++ Linked List.py
@@ -0,0 +1,158 @@
+class Node:
+    def __init__(self, data= 0, next= 0):
+        self.data = data
+        self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_beginning(self, data):
+        node = Node(data, self.head)
+        self.head = node
+
+    def print(self):
+        if self.head is None:
+            print("Linked list empty")
+            return
+
+        itr = self.head
+        llstr = ''
+
+        while itr:
+            llstr += str(itr.data) + '-->'
+            itr = itr.next
+
+
+        print(llstr)
+
+    def insert_at_end(self,data):
+        if self.head is None:
+            self.head = Node(data, None)
+            return
+
+        itr = self.head
+        while itr.next:
+            itr = itr.next
+
+        itr.next = Node(data, None)
+
+    def insert_value(self, data_list):
+        self.head = None
+        for data in data_list:
+            self.insert_at_end(data)
+
+    def get_length(self):
+        count = 0
+        itr = self.head
+        while itr:
+            count += 1
+            itr = itr.next
+
+        return count
+
+    def insert_after_value(self, data_after, data_to_insert):
+        if self.head is None:
+            return
+
+        if self.head.data==data_after:
+            self.head.next = Node(data_to_insert,self.head.next)
+            return
+
+        itr = self.head
+        while itr:
+            if itr.data == data_after:
+                itr.next = Node(data_to_insert, itr.next)
+                break
+
+            itr = itr.next
+
+    def remove_at(self, index):
+        if index<0 or index>=self.get_length():
+            raise Exception("Invalid Index")
+
+        if index==0:
+            delf.head = self.head.next
+            return
+
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                itr.next = itr.next.next
+                break
+
+            itr = itr.next
+            count += 1
+
+    def insert_at(self,index,data):
+        if index <0 or index>self.get_length():
+            raise Exception("Invalid Index")
+
+        if index ==0:
+            self.insert_at_beginning(data)
+            return
+
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                node = Node(data,itr.next)
+                its.next = node
+                break
+
+            itr = itr.next
+            count += 1
+
+    def remove_by_value(self, data):
+        if self.head is None:
+            return
+
+        if self.head.data == data:
+            self.head = self.head.next
+            return
+
+        itr = self.head
+        while itr.next:
+            if itr.next.data == data:
+                itr.next = itr.next.next
+                break
+            itr = itr.next
+
+if __name__ == '__main__':
+    ll = LinkedList()
+    ll.insert_value(["banana","mango","grapes","orange"])
+    ll.print()
+    ll.insert_after_value("mango","apple")
+    ll.print()
+    ll.remove_by_value("orange")
+    ll.print()
+    ll.remove_by_value("figs")
+    ll.print()
+    ll.remove_by_value("banana")
+    ll.remove_by_value("mango")
+    ll.remove_by_value("apple")
+    ll.remove_by_value("grapes")
+    ll.print()
+
+"""
+if __name__ == '__main__':
+    ll = LinkedList()
+    ll.insert_value([47,7,12,567,99])
+    ll.insert_at_end(67)
+    ll.print()
+    ll.remove_at(2)
+    ll.print()
+
+    ll.insert_value(["banana","mango","grapes","orange"])
+    #ll.insert_at_beginning(5)
+    #ll.insert_at_beginning(89)
+    #ll.insert_at_end(9876)
+    ll.print()
+    #ll.remove_at(2)
+    ll.insert_at(0,"figs")
+    ll.print()
+    ll.insert_at(2,"jackfruit")
+    ll.print()
+    #print("length:",ll.get_length())
+    """
